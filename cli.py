fae9a63988c6ada32c85a15cc2b6c51038342388
zengzhifei <zengzhifei@outlook.com>
2025-02-18 22:53:45 +0800

    update bp_sub

--- cli.py
+++ cli.py
@@ -1595,7 +1595,7 @@ def bp_sub():
     parser = argparse.ArgumentParser()
     parser.add_argument("--cluster", type=str, required=True)
     parser.add_argument("--meta_host", type=str, required=True)
-    parser.add_argument("--pack", action="store_true")
+    parser.add_argument("--unpack", action="store_true")
     parser.add_argument("--pipename", type=str, required=True)
     parser.add_argument("--username", type=str, required=True)
     parser.add_argument("--password", type=str, required=True)
@@ -1613,7 +1613,7 @@ def bp_sub():
                               r'(root_path:\s*)(.*)', lambda m: f"{m.group(1)}{cluster}")
     sdk.modify_file_by_patten(str(Path("./conf/bigpipe.conf")),
                               r'(meta_host:\s*)(.*)', lambda m: f"{m.group(1)}{args.meta_host}")
-    pack = 'True' if args.pack else 'False'
+    pack = 'False' if args.unpack else 'True'
     sdk.modify_file_by_patten(str(Path("./conf/gflags.conf")),
                               r'(--pack=\s*)(.*)', lambda m: f"{m.group(1)}{pack}")
     sdk.modify_file_by_patten(str(Path("./conf/bp.conf")),
