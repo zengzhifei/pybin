acb7c3ae33353613a93f7fc3447e9e15d26c5150
zengzhifei <zengzhifei@outlook.com>
2026-05-17 02:50:30 +0800

    release: v1.0.26

--- pybin/__about__.py
+++ pybin/__about__.py
@@ -1,5 +1,5 @@
 __name__ = "pybin"
-__version__ = "1.0.25"
+__version__ = "1.0.26"
 __author__ = "zengzhifei"
 __author_email__ = "zengzhifei@outlook.com"
 __doc__ = "A tool SDK and CLI provided for the Python command line"
